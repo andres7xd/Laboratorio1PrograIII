13800f9fcc072de4f17309161d5acbec26b93c2b
andres7xd <58347580+andres7xd@users.noreply.github.com>
2020-09-12 20:31:20 -0600

    clase main

---
+++ LaboratorioPIII/src/laboratoriopiii/LaboratorioPIII.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratoriopiii;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+
+
+/**
+ *
+ * @author andre
+ */
+public class LaboratorioPIII extends Application{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
